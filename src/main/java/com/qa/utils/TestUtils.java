d834a3526e6284fb46274aef871ba09d84b45aeb
itsaqibkhan <mohd.aqib9811@gmail.com>
2023-11-26 13:21:51 +0530

    Copied the config, log4j2 and testutils files from the practiceFrameWork and added the app as well

---
+++ src/main/java/com/qa/utils/TestUtils.java
@@ -0,0 +1,14 @@
+package com.qa.utils;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestUtils {
+    public static final int WAIT = 10;
+
+
+    public Logger log() {
+        return LogManager.getLogger(Thread.currentThread().getStackTrace()[2].getClassName());
+    };
+
+}
